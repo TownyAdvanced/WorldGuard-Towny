4de8e164f5a46a843e7ecf00b9e40aa55b3e194d
suneettipirneni <suneettipirneni@icloud.com>
2020-01-03 20:34:27 -0700

    Cleanup code and add event for claims.

--- src/main/java/TownyListener.java
+++ src/main/java/TownyListener.java
@@ -1,5 +1,7 @@
 
+import com.palmergames.bukkit.towny.TownyMessaging;
 import com.palmergames.bukkit.towny.event.PreNewTownEvent;
+import com.palmergames.bukkit.towny.event.TownPreClaimEvent;
 import com.sk89q.worldedit.bukkit.BukkitAdapter;
 import com.sk89q.worldedit.math.BlockVector3;
 import com.sk89q.worldedit.world.World;
@@ -11,6 +13,8 @@ import com.sk89q.worldguard.protection.managers.RegionManager;
 import com.sk89q.worldguard.protection.regions.RegionContainer;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
@@ -20,7 +24,17 @@ public class TownyListener implements Listener {
     public void onTownCreate(PreNewTownEvent event) {
         // Get the player.
         Player player = event.getPlayer();
+        validateAction(player, event);
+    }
 
+    @EventHandler
+    public void onTownClaim(TownPreClaimEvent event) {
+        // Get the player.
+        Player player = event.getPlayer();
+        validateAction(player, event);
+    }
+
+    public void validateAction(Player player, Cancellable event) {
         // Convert properties into WorldGuard compatible objects.
         BlockVector3 loc = BukkitAdapter.asBlockVector(player.getLocation());
         World world = BukkitAdapter.adapt(player.getWorld());
@@ -41,7 +55,7 @@ public class TownyListener implements Listener {
         // Check the flag.
         if (!set.testState(wgPlayer, WorldGuardTowny.getTownCreationAllowedFlag())) {
             // Cancel Town creation
-            event.setCancelMessage(ChatColor.RED + "[WorldGuard-Towny]" + "You're not allowed to create a town in this region.");
+            TownyMessaging.sendErrorMsg(player, "You're not allowed to claim in this region.");
             event.setCancelled(true);
         }
     }
